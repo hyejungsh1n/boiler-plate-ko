c8dd0e42985fe5fa9a4e9ec1d2577d6f3fcb9881
shin <kahkii@naver.com>
2022-03-13 05:02:47 +0900

    redux update

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from "redux";
+// import user from './user_reducer';
+
+const rootReducer = combineReducers({
+    // user,
+})
+export default rootReducer;
+// 다른 파일에서 reducer를 쓸 수 있게
+
+
+
+/* 여러가지 reducer를 하나로 합쳐줌 */
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { ProVider } from 'react-redux';
 import 'antd/dist/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
 
+const createStoreMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 ReactDOM.render(
-<App />,
-  document.getElementById('root')
+  <Provider
+    store={createStoreMiddleware(Reducer, 
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION()
+    )}>
+    <App />
+  </Provider>  
+  ,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
