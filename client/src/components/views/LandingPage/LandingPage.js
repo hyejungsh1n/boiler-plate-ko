1dd0392b61c9b2c72b1f5e0ad78f0a8152011dd6
shin <kahkii@naver.com>
2022-03-19 21:29:02 +0900

    로그아웃 기능 구현, 로그아웃 성공시 페이지 이동

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -9,13 +9,25 @@ function LandingPage() {
         .then(response => console.log(response.data))
     }, [])
 
+
+    const onClickHandler = () => {
+        axios.get(`/api/users/logout`)
+        .then(response => { 
+            if(response.data.success) {
+                navigate("/login")
+            } else {
+                alert("로그아웃 하는데 실패했습니다.")
+            }
+        })
+
+    }
     return (
         <div style={{
             display: 'flex', justifyContent: 'center', alignItems:'center',
             width: '100%', height: '100vh'
         }}>
             <h2> 환영합니다..</h2>
-            <button>
+            <button onclick={onClickHandler}>
                 로그아웃
             </button>
         </div>
