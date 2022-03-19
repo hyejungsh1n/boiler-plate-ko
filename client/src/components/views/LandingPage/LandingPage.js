e48f1a97a84a23788c0f0289818b4a82994c088a
shin <kahkii@naver.com>
2022-03-20 04:12:48 +0900

    랜딩페이지 오류, 버튼 에러 수정

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function LandingPage() {
-
+    let navigate = useNavigate();
 
     useEffect(() => {
         axios.get('/api/hello')
@@ -11,10 +12,10 @@ function LandingPage() {
 
 
     const onClickHandler = () => {
-        axios.get(`/api/users/logout`)
+        axios.get('/api/users/logout')
         .then(response => { 
             if(response.data.success) {
-                navigate("/login")
+                navigate('/login')
             } else {
                 alert("로그아웃 하는데 실패했습니다.")
             }
@@ -27,7 +28,7 @@ function LandingPage() {
             width: '100%', height: '100vh'
         }}>
             <h2> 환영합니다..</h2>
-            <button onclick={onClickHandler}>
+            <button onClick={onClickHandler}>
                 로그아웃
             </button>
         </div>
