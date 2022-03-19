c9623242ccb2e1bacfd2beb8e94e9853ecdd4185
shin <kahkii@naver.com>
2022-03-20 04:27:38 +0900

    분기 처리

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -2,14 +2,32 @@ import React, { useEffect } from 'react';
 import Axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { auth } from '../_actions/user_action'
+import { useNavigate } from 'react-router-dom';
 export default function (SpectificComponent, option, adminRoute = null) {
 
     function AuthenticationCheck(props) {
     
+        let navigate = useNavigate();
         const dispatch = useDispatch();
         useEffect(() => {
         dispatch(auth()).then(response => {
             console.log(response)
+
+            // 로그인 하지 않은 상태
+            if(!response.payload.isAuth) {
+                if(option) {
+                    navigate('/login')
+                }
+            } else {
+                // 로그인 한 상태
+                if(adminRoute && !response.payload.isAdmin) {
+                    navigate('/')   
+                } else{
+                    if(option === false) {
+                        navigate('/')
+                    }
+                }
+            }
         })
         // Axios.get('/api/users/auth')
     }, [])
