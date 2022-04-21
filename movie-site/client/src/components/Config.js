74e19016dba37e89d7b39a4208dec58bffaa3989
hj <kahkii@naver.com>
2022-04-21 16:51:03 +0900

    update

--- movie-site/client/src/components/Config.js
+++ movie-site/client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+// api 상수 처리
+export const API_URL = 'https://api.themoviedb.org/3/' //영화 정보
+export const IMAGE_BASE_URL = 'http//image.tmdb.org/t/p/' // 이미지 정보
