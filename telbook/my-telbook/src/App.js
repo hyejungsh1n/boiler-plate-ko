3db79573687758600cb22683f29c41957c8db4b1
hj <kahkii@naver.com>
2022-05-30 16:19:20 +0900

    except unnecessary comment

--- telbook/my-telbook/src/App.js
+++ telbook/my-telbook/src/App.js
@@ -4,7 +4,6 @@ import PhoneList from "./components/PhoneList"
 import PhoneItem from "./components/PhoneItem"
 import "./App.css";
 
-// class component 강의를 들어서 이렇게 되어 있음 
 
 function App() {
   return (
