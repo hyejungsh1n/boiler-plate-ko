f037dada7d76a68353fccedc289d86f5e0c8a84e
hj <kahkii@naver.com>
2022-05-30 15:15:38 +0900

    InputBox 분리

--- telbook/my-telbook/src/App.js
+++ telbook/my-telbook/src/App.js
@@ -6,27 +6,7 @@ import "./App.css";
 function App() {
   return (
     <div className="container">
-      <div className="input_boxes">
-        <div className="input_box">
-          <div className="input_box_name">이름</div>
-          <input
-            type="text"
-            placeholder="이름"
-            name="name"
-            className="input_box_input"
-          />
-        </div>
-        {/* <div className="input_box">
-          <div className="input_box_name">전화번호</div>
-          <input
-            type="text"
-            placeholder="전화번호"
-            name="phone"
-            className="input_box_input"
-          />
-        </div>
-        <button className="input_box_button">저장</button> */}
-      </div>
+
 
       <div className="phone_list">
         <div className="phone_item">
