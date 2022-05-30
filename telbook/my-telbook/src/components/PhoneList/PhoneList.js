b8aa777ad0e7c81df2df0a5effd84928155a216f
hj <kahkii@naver.com>
2022-05-30 16:15:14 +0900

    seperating PhoneItem

--- telbook/my-telbook/src/components/PhoneList/PhoneList.js
+++ telbook/my-telbook/src/components/PhoneList/PhoneList.js
@@ -8,33 +8,7 @@ import "./PhoneList.scss"
 const PhoneList = () => {
     return (
         <div className="phone_list">
-        <div className="phone_item">
-          <div className="phone_item_left">
-            <div className="phone_item_name">홍길동</div>
-            <div className="phone_item_phone">010-0000-0000</div>
-          </div>
-          <div className="phone_item_right">
-            <button>삭제</button>
-          </div>
-        </div>
-        <div className="phone_item">
-          <div className="phone_item_left">
-            <div className="phone_item_name">홍길동</div>
-            <div className="phone_item_phone">010-0000-0000</div>
-          </div>
-          <div className="phone_item_right">
-            <button>삭제</button>
-          </div>
-        </div>
-        <div className="phone_item">
-          <div className="phone_item_left">
-            <div className="phone_item_name">홍길동</div>
-            <div className="phone_item_phone">010-0000-0000</div>
-          </div>
-          <div className="phone_item_right">
-            <button>삭제</button>
-          </div>
-        </div>
+
       </div>
     );
 }
