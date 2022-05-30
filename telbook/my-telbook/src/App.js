83d348abe9843a1959ad4daefde2b58382886821
hj <kahkii@naver.com>
2022-05-30 16:04:09 +0900

    return Component PhoneList

--- telbook/my-telbook/src/App.js
+++ telbook/my-telbook/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import InputBox from "./components/InputBox"
+import PhoneList from "./components/PhoneList"
 import "./App.css";
 
 // class component 강의를 들어서 이렇게 되어 있음 
@@ -10,35 +11,9 @@ function App() {
 
       <InputBox />
 
-      <div className="phone_list">
-        <div className="phone_item">
-          <div className="phone_item_left">
-            <div className="phone_item_name">홍길동</div>
-            <div className="phone_item_phone">010-0000-0000</div>
-          </div>
-          <div className="phone_item_right">
-            <button>삭제</button>
-          </div>
-        </div>
-        <div className="phone_item">
-          <div className="phone_item_left">
-            <div className="phone_item_name">홍길동</div>
-            <div className="phone_item_phone">010-0000-0000</div>
-          </div>
-          <div className="phone_item_right">
-            <button>삭제</button>
-          </div>
-        </div>
-        <div className="phone_item">
-          <div className="phone_item_left">
-            <div className="phone_item_name">홍길동</div>
-            <div className="phone_item_phone">010-0000-0000</div>
-          </div>
-          <div className="phone_item_right">
-            <button>삭제</button>
-          </div>
-        </div>
-      </div>
+      <PhoneList />
+
+
     </div>
   );
 }
