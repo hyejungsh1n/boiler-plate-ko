f54d845eea9ec6cd09ca14b9039b36162073608d
hj <kahkii@naver.com>
2022-05-30 16:15:52 +0900

    add Component PhoneItem

--- telbook/my-telbook/src/App.js
+++ telbook/my-telbook/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import InputBox from "./components/InputBox"
 import PhoneList from "./components/PhoneList"
+import PhoneItem from "./components/PhoneItem"
 import "./App.css";
 
 // class component 강의를 들어서 이렇게 되어 있음 
@@ -12,6 +13,8 @@ function App() {
       <InputBox />
 
       <PhoneList />
+
+      <PhoneItem />
 
 
     </div>
