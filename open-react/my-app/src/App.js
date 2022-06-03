fe3fe188957516015d2b00b65f2cf004e5365352
hj <kahkii@naver.com>
2022-06-03 16:10:17 +0900

    TOC state 설정

--- open-react/my-app/src/App.js
+++ open-react/my-app/src/App.js
@@ -13,7 +13,13 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      subject:{title : 'WEB', sub: 'World Wide Web!'}
+      subject:{title : 'WEB', sub: 'World Wide Web!'},
+      contents: [
+        {id : 1, title:'HTML', desc:'HTML is for information'},
+        {id : 2, title:'CSS', desc:'Css is for design'},
+        {id : 3, title:'Javascript', desc:'Javascript is interactive'}
+
+      ]
       
     }
   }
@@ -23,8 +29,8 @@ class App extends Component {
     <div className="App">
       <Subject title={this.state.subject.title} 
       sub={this.state.subject.sub}></Subject>
-      <TOC></TOC>
-      <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+      <TOC data={this.state.contents}></TOC>
+      <Content></Content>
     </div>
     )
   }
