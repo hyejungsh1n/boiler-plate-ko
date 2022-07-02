74edd045f2f6aca6420f457880a49a6a7d9ef9e0
shin <kahkii@naver.com>
2022-07-02 16:28:09 +0900

    Add function in link and button

--- open-react/my-app/src/components/Control.js
+++ open-react/my-app/src/components/Control.js
@@ -4,11 +4,22 @@ class Subject extends Component {
     render() {
         console.log('Control render');
         return (
+
         <ul>
-          <li><a href="/create">create</a></li>
-          <li><a href="/update">update</a></li>
-          <li><input type="button" value="delete"></input></li>
+          <li><a href="/create" onClick={function(e) {
+                e.preventDefault();
+                this.props.onChangeMode('create');
+            }.bind(this)}>create</a></li>
+          <li><a href="/update" onClick={function(e) {
+              e.preventDefault();
+              this.props.onChangeMode('update')
+          }.bind(this)}>update</a></li>
+          <li><input onClick={function(e){
+              e.preventDefault();
+              this.props.onChangeMode('delete');
+          }.bind(this)} type="button" value="delete"></input></li>
         </ul>
+
         )
     }
 }
